ff2f636c0f2c11b71863712e9485639685aaa9d6
Рауан Жорабек <rauan@rauans-macbook-pro.local>
2019-07-23 12:34:43 -0400

    final version

--- Habit-Calendar/Model/HCPersistentContainer.swift
+++ Habit-Calendar/Model/HCPersistentContainer.swift
@@ -9,7 +9,7 @@
 import CoreData
 
 class HCPersistentContainer: NSPersistentContainer {
-
+  
     override class func defaultDirectoryURL() -> URL {
         guard let appGroupContainerUrl = FileManager.default.containerURL(
             forSecurityApplicationGroupIdentifier: "group.essentialism"
